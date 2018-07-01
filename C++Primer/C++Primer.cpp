60bf203f39f058d867675b65368ddb4318abe282
gaoku <gaoku@DESKTOP-TEO8VH8>
2018-07-01 19:54:29 +0200

    Add project files.

---
+++ C++Primer/C++Primer.cpp
@@ -0,0 +1,16 @@
+// C++Primer.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+	string word;
+	while (cin >> word)
+		cout << word << endl;
+	return 0;
+}
+
